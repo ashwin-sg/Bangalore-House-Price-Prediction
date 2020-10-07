dea20e4d615362d01fef787490d7385dd0cb518f
ashwin-sg <65702360+ashwin-sg@users.noreply.github.com>
2020-10-07 06:40:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,12 @@
-  
 mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"your-email@domain.com\"\n\
+" > ~/.streamlit/credentials.toml
 
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
